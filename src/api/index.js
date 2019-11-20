369b7bd64c67af099b58b836b1a370cf524cdba0
ILWOO <dlfdn91@gmail.com>
2019-11-20 15:39:39 +0900

    change base server url

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 const api = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_ADDRESS
+  baseURL: "https://api.myhiddenplaces.com"
 });
 
 export const confirmUser = () => {
